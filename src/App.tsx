ae038edac840b9e0d4b2e1befbdebe61d80e78c6
Ben Rollin <ben.rollin@clever.com>
2020-09-28 18:56:41 -0700

    World capitalizer stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+const { countries, capitals } = require("./data.json");
+
+function checkAnswerText(answerText: string, answer: string): boolean {
+  return answerText.toLowerCase().trim() === answer.toLowerCase().trim();
+}
+
+function TestComponent() {
+  const [countryIndex, setCountryIndex] = useState(0);
+  const [answerText, setAnswerText] = useState("");
+  useEffect(() => {
+    console.log("using effect");
+  }, [countryIndex]);
+  return <div>
+    Previous Country: {countryIndex > 0 && countries[countryIndex - 1]}
+    <br />
+    Previous Capital: {countryIndex > 0 && capitals[countryIndex - 1]}
+    <br />
+    <br />
+    Country: {countries[countryIndex]}
+    <br />
+    <input
+      type="text"
+      onChange={e => {
+        const newAnswer = e.target.value;
+        if (checkAnswerText(newAnswer, capitals[countryIndex])) {
+          setAnswerText("");
+          setCountryIndex(countryIndex + 1);
+          return;
+        }
+
+        setAnswerText(e.target.value);
+      }}
+      value={answerText}
+    />
+    <button
+      onClick={() => setCountryIndex(countryIndex + 1)}
+    >Next</button>
+  </div>;
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <TestComponent />
       </header>
     </div>
   );
