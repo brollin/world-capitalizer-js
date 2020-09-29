fff1ccb3f64ceb80b7c81a6b84b873f9300feb29
Ben Rollin <ben.rollin@clever.com>
2020-09-28 19:16:49 -0700

    Add map

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import logo from './World-Map.svg';
 import './App.css';
 
 const { countries, capitals } = require("./data.json");
@@ -9,36 +9,44 @@ function checkAnswerText(answerText: string, answer: string): boolean {
 }
 
 function TestComponent() {
+  const [correctCount, setCorrectCount] = useState(0);
   const [countryIndex, setCountryIndex] = useState(0);
   const [answerText, setAnswerText] = useState("");
-  useEffect(() => {
-    console.log("using effect");
-  }, [countryIndex]);
+
+  function handleAnswerChange(newAnswer: string) {
+    if (checkAnswerText(newAnswer, capitals[countryIndex])) {
+      setAnswerText("");
+      setCountryIndex(countryIndex + 1);
+      setCorrectCount(correctCount + 1);
+      return;
+    }
+
+    setAnswerText(newAnswer);
+  }
+
   return <div>
+    <div style={{ color: "cyan" }}>
+      Correct: {correctCount} / {countryIndex}
+    </div>
+    <br />
     Previous Country: {countryIndex > 0 && countries[countryIndex - 1]}
     <br />
     Previous Capital: {countryIndex > 0 && capitals[countryIndex - 1]}
     <br />
     <br />
-    Country: {countries[countryIndex]}
+    What is the capital of <span style={{color: "lightgreen" }}>{countries[countryIndex]}</span>?
     <br />
     <input
+      autoFocus
       type="text"
       onChange={e => {
-        const newAnswer = e.target.value;
-        if (checkAnswerText(newAnswer, capitals[countryIndex])) {
-          setAnswerText("");
-          setCountryIndex(countryIndex + 1);
-          return;
-        }
-
-        setAnswerText(e.target.value);
+        handleAnswerChange(e.target.value);
       }}
       value={answerText}
     />
     <button
       onClick={() => setCountryIndex(countryIndex + 1)}
-    >Next</button>
+    >I don't know :(</button>
   </div>;
 }
 
